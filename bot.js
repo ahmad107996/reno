cc243783754fc3d91ea5f2d14b20915b4d18efe7
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2019-01-02 15:27:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,30 @@ client.on('ready', () => {
 
 // Toxic Codes
 
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها :ok_hand:```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
 
 client.on("message", message => {
     if (!message.content.startsWith(prefix)) return;
